2df3cec279bdf1aca161173625d3d4abf4e68eec
Oriol Augé <8628021+oriolauge@users.noreply.github.com>
2024-11-17 19:15:08 +0100

    Add fixes to adapt module to PHP 8.3

--- Ui/DataProvider/EavValidationRules.php
+++ Ui/DataProvider/EavValidationRules.php
@@ -5,7 +5,7 @@ use OAG\Blog\Api\Data\EavAttributeInterface;
 
 /**
  * Class build validation rules for OAG Blog EAV attributes
- * 
+ *
  * Based on Magento\Catalog\Ui\DataProvider\CatalogEavValidationRules;
  */
 class EavValidationRules
@@ -27,14 +27,18 @@ class EavValidationRules
             $rules['validate-zero-or-greater'] = true;
         }
 
-        $validationClasses = explode(' ', $attribute->getFrontendClass());
+        $validationClasses = $attribute->getFrontendClass()
+            ? explode(' ', $attribute->getFrontendClass())
+            : [];
 
         foreach ($validationClasses as $class) {
             if (preg_match('/^maximum-length-(\d+)$/', $class, $matches)) {
+                // phpcs:ignore Magento2.Performance.ForeachArrayMerge
                 $rules = array_merge($rules, ['max_text_length' => $matches[1]]);
                 continue;
             }
             if (preg_match('/^minimum-length-(\d+)$/', $class, $matches)) {
+                // phpcs:ignore Magento2.Performance.ForeachArrayMerge
                 $rules = array_merge($rules, ['min_text_length' => $matches[1]]);
                 continue;
             }
@@ -59,6 +63,7 @@ class EavValidationRules
             case 'validate-digits':
             case 'validate-email':
             case 'validate-url':
+            case 'validate-trailing-hyphen':
             case 'validate-alpha':
             case 'validate-alphanum':
                 $rules = array_merge($rules, [$class => true]);
